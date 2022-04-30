0093ea435ffdf9ce3fc080430a884e78d4b177ae
juan20carlos <juancarlos14089@gmail.com>
2022-04-30 17:28:24 +0000

    Version 2

--- 0x00-python-hello_world/5-print_string.py
+++ 0x00-python-hello_world/5-print_string.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Holberton School"
-print(f"{str}" +
-      f"{str}" +
-      f"{str}")
+print(f"{str}" + f"{str[0:9]}" +
+      f"{str[9:]}" + f"{str[0:9]}" +
+      f"{str[9:]}" + f"{str[0:9]}")
