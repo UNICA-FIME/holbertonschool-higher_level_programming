3694112f59a1c55af63fffe0d156232d83f159f1
juan20carlos <juancarlos14089@gmail.com>
2022-05-17 05:08:52 +0000

    Version 1 file

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    z = 0
+    k = 0
+    try:
+        for i in my_list[:x]:
+            print("{0}".format(i), end="")
+            z = z + 1
+        print()
+        return (l)
+    except IndexError:
+        for a in my_list:
+            print("{0}".format(a), end="")
+            k = k + 1
+        print()
+        return k
