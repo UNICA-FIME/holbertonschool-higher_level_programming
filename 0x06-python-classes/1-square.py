cc6f209bc501ba695b9fcbc5a16eb473ff11ad35
juan20carlos <juancarlos14089@gmail.com>
2022-05-21 22:31:36 +0000

    Version 1 1-square.py file

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Square with size"""
+
+
+class Square:
+    """Class Square that defines a square
+
+    Attributes:
+        _size (int): size of a side of the square
+    """
+    def __init__(self, size):
+        """ Initializes a square
+
+        Args:
+            sie (int): size of a side of the square
+
+        Return: None
+        """
+        self.__size = size
