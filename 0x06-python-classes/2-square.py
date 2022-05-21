95073f14d0be889060d869f6988bd23d46d7302c
juan20carlos <juancarlos14089@gmail.com>
2022-05-21 23:54:14 +0000

    Version 1 2-square.py

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" class Square definition"""
+
+
+class Square:
+    """ class Square that defines a square"""
+
+    def __init__(self, size=0):
+        """Initilize a square
+        Attributtes:
+         - size(Privete): size of the square
+
+         First determinate if the value of the size is right,
+          integer and positive
+         """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__seze = size
