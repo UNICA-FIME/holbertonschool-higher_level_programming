cf60eacc1fc188b914f6fd00ca127637cfcbf232
juan20carlos <juancarlos14089@gmail.com>
2022-05-22 22:37:27 +0000

    Version first 0-square_matrix_simple.py file

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+def square_matrix_simple(matrix=[]):
+    f = len(matrix)
+    c = len(matrix[0])
+    matrix_cp = [x[:] for x in matrix]
+    for i in range(f):
+        for j in range(c):
+            matrix_cp[i][j] = matrix_cp[i][j]**2
+    return(matrix_cp)
