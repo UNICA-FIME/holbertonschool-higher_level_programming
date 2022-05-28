7eda67f9f4dbfd506c779dfb2c121c060f11c9aa
juan20carlos <juancarlos14089@gmail.com>
2022-05-28 16:31:01 +0000

    Version 1 1-rectangle.py file

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -20,7 +20,7 @@ class Rectangle:
     def width(self, value):
         """change value of attribute self.__with"""
 
-        if type(value) != int:
+        if type(value) is not int:
             raise TypeError("width must be an integer")
         elif value < 0:
             raise ValueError("width must be >= 0")
@@ -36,7 +36,7 @@ class Rectangle:
     def height(self, value):
         """Change value of attribute self.__height"""
 
-        if type(value) != int:
+        if type(value) is not int:
             raise TypeError("height must be an integer")
         elif value < 0:
             raise ValueError("height must be >= 0")
