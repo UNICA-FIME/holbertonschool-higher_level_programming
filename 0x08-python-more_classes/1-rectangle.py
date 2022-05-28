965466aa80070b2a862160a3b6b6d8e88b145e91
juan20carlos <juancarlos14089@gmail.com>
2022-05-28 22:03:05 +0000

    Version firts

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -15,21 +15,17 @@ class Rectangle:
     @property
     def width(self):
         """Access the attribute self.with"""
-        return self.__witdh
+        return self.__width
 
     @width.setter
     def width(self, value):
         """change value of attribute self.__with"""
-        try:
-            if type(value) is not int:
-                raise TypeError("width must be an integer")
-            if value < 0:
-                raise ValueError("width must be >= 0")
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
             self.__width = value
-        except TypeError as error:
-            print(error)
-        except ValueError as error1:
-            print(error1)
 
     @property
     def height(self):
@@ -39,13 +35,9 @@ class Rectangle:
     @height.setter
     def height(self, value):
         """Change value of attribute self.__height"""
-        try:
-            if type(value) is not int:
-                raise TypeError("height must be an integer")
-            if value < 0:
-                raise ValueError("height must be >= 0")
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
             self.__height = value
-        except TypeError as error:
-            print(error)
-        except ValueError as error1:
-            print(error1)
