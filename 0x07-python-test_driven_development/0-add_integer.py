5e40b6666fa0a759b057aaa474dac96558a7439e
UNICA-FIME <juancarlos14089@gmail.com>
2022-06-13 05:35:00 +0000

    Version 1 file

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+This module add two integers or floats
+Use the add_integer(a, b) with a and b how integer or floats
+like: add_integer(1, 10)
+return: 11
+"""
+
+
+def add_integer(a, b=98):
+    """Return the sum of a + b
+    Args: a: integer or float
+          b: integer or float
+    """
+    if type(a) != int and type(a) != float:
+        raise TypeError("a must be an integer")
+    if type(b) != int and type(b) != float:
+        raise TypeError("b must be an integer")
+    if type(a) == float:
+        a = int(a)
+    if type(b) == float:
+        b = int(b)
+    return (a + b)
