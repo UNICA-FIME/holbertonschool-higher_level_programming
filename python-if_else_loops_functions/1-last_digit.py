2ed29f18fac31ec6a08268e00d5c111c4d9bdf83
UNICA-FIME <juancarlosfime@gmail.com>
2022-09-06 18:28:05 +0000

    correct error

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -3,7 +3,7 @@ import random
 number = random.randint(-10000, 10000)
 if (number > 0):
     last_dig = number % 10
-elif(number < 0):
+else:
     last_dig = (-1)*((-1) * number % 10)
 if (last_dig > 5):
     print(f"Last digit of {number} is {last_dig} and is greater than 5")
