f24e4b960c86d1f016ab7e2cedbe4e74ec0ecb1b
UNICA-FIME <juancarlosfime@gmail.com>
2022-09-06 23:54:24 +0000

    combinations of two digits

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -2,7 +2,7 @@
 i = 1
 while (i < 100):
     if (i % 10 != 0 and i < 10):
-        print("{:02d}".format(i)i, end=", ")
+        print("{:02d}".format(i), end=", ")
     elif ((i % 10) > (i / 10) and (i % 11) != 0 and i > 10 and i != 89):
         print("{:02d}".format(i), end=", ")
     elif (i == 89):
