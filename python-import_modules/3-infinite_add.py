67d031f842f547455ff027d24e535cadb02b80d2
UNICA-FIME <juancarlosfime@gmail.com>
2022-09-09 02:28:41 +0000

    correction error

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -3,8 +3,9 @@ from sys import argv, stdout
 num_arg = len(argv)
 i = 1
 sum_arg = 0
-if (num_arg > 2):
-    while (i < num_arg):
-        sum_arg = sum_arg + int(argv[i])
-        i += 1
-stdout.write("{}\n".format(sum_arg))
+if __name__ == "__main__":
+    if (num_arg > 2):
+        while (i < num_arg):
+            sum_arg = sum_arg + int(argv[i])
+            i += 1
+            stdout.write("{}\n".format(sum_arg))
