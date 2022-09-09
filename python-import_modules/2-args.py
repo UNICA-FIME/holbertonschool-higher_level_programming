9ff15ba660c69f327dbfe55c3a59b8ecc0524789
UNICA-FIME <juancarlosfime@gmail.com>
2022-09-09 01:52:58 +0000

    prints the number of arguments.

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,13 +2,14 @@
 from sys import argv, stdout
 i = 1
 num_arg = len(argv) - 1
-if (num_arg == 0):
-    stdout.write("{:d} arguments{}\n".format(num_arg, "."))
-elif (num_arg == 1):
-    stdout.write("{:d}{:s}argument{:s}\n".format(num_arg, " ", ":"))
-    stdout.write("{:d}: {:s}\n".format(num_arg, argv[num_arg]))
-else:
-    stdout.write("{:d} arguments:\n".format(num_arg))
-    while (i < num_arg):
-        stdout.write("{:d} {}\n".format(i, argv[i + 1]))
-        i += 1
+if __name__ == "__main__":
+    if (num_arg == 0):
+        stdout.write("{:d} arguments{}\n".format(num_arg, "."))
+    elif (num_arg == 1):
+        stdout.write("{:d}{:s}argument{:s}\n".format(num_arg, " ", ":"))
+        stdout.write("{:d}: {:s}\n".format(num_arg, argv[num_arg]))
+    else:
+        stdout.write("{:d} arguments:\n".format(num_arg))
+        while (i < num_arg):
+            stdout.write("{:d}: {}\n".format(i, argv[i + 1]))
+            i += 1
