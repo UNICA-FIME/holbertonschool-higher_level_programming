8ab3546a762423473e59c3b486dc242108c85b47
UNICA-FIME <juancarlosfime@gmail.com>
2022-09-12 20:33:04 +0000

    case empty list

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     len_list = len(my_list)
-    if (idx < 0):
+    if (idx < 0 or len_list == 0):
         return (None)
     if (idx >len_list):
         return (None)
