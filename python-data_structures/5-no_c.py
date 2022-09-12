8ca49a362faeb097abdfb230631f431c8c8643ed
UNICA-FIME <juancarlosfime@gmail.com>
2022-09-12 23:43:45 +0000

    correction error

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new_list = []
+    new_list_1 = []
     new_list = list(my_string)
     for i in new_list:
-        if i in "Cc":
-            new_list.remove(i)
-    return ("".join(new_list))
+        if (i != 'c' and i != 'C'):
+            new_list_1.append(i)
+
+    return ("".join(new_list_1))
