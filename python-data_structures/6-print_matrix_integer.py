8ed55655c215c66531be49a1307fd3c6e3f70494
UNICA-FIME <juancarlosfime@gmail.com>
2022-09-13 01:36:03 +0000

    prints a matrix of integers.

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -6,8 +6,8 @@ def print_matrix_integer(matrix=[[]]):
         t = 1
         for j in matrix_1:
             if (t < count):
-                print("{}".format(j), end=" ")
+                print("{:d}".format(j), end=" ")
             else:
-                print("{}".format(j))
+                print("{:d}".format(j))
             t += 1
         t = 0
