267d910a231a3289e54e2b97f1954bf8870d531a
UNICA-FIME <juancarlosfime@gmail.com>
2022-09-14 23:58:51 +0000

    returns a key with the biggest integer value

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if(a_dictionary is None):
+        return (None)
+    key = a_dictionary.keys()
+    j = 0
+    key_max = ""
+    for i in key:
+        if a_dictionary[i] > j:
+            j = a_dictionary[i]
+            key_max = i
+    return key_max
