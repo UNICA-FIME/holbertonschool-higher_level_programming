8aa7f2858ff08995c9759f8a6a4f325cd54f2a3a
UNICA-FIME <juancarlosfime@gmail.com>
2022-09-15 15:49:26 +0000

    returns the weighted average

--- python-more_data_structures/100-weight_average.py
+++ python-more_data_structures/100-weight_average.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
+    if (len(my_list) == 0):
+        return (0)
     list_1 = list(map(lambda x: x[0]*x[1], my_list))
     list_2 = list(map(lambda y: y[1], my_list))
     return (sum(list_1) / sum(list_2))
