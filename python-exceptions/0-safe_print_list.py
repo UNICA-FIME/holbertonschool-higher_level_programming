bafd60498580353929e167ae71004fa079bb1122
UNICA-FIME <juancarlosfime@gmail.com>
2022-09-17 15:42:01 +0000

    correction error of index

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     try:
-        for i in range(1, x+1):
-            print("{}".format(my_list[i - 1]), end="")
+        j = 0
+        for i in range(0, x):
+            print("{}".format(my_list[i]), end="")
+            j = j + 1
         print()
     except IndexError:
-        i = i - 1
         print()
-    return (i)
+    return (j)
