72cce9452516fce0e7b7ceae08e8cc51c9e9fb37
UNICA-FIME <juancarlosfime@gmail.com>
2022-09-18 15:40:11 +0000

    function that prints an integer

---
+++ python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+
+
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+        rsp = True
+    except ValueError as err:
+        sys.stderr.write("Exception: {}\n".format(err))
+        rsp = False
+    finally:
+        return (rsp)
