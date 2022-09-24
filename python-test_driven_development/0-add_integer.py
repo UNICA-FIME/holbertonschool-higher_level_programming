e372dca5a2c93292fc1ee20f9d5ce0fee05887b4
UNICA-FIME <juancarlosfime@gmail.com>
2022-09-24 22:11:09 +0000

    function that adds 2 integers

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" This is module implement function
+for adds 2 integers.
+"""
+import doctest
+
+
+def add_integer(a, b=98):
+    """This is function to add 2 integer
+
+    Args:
+        a: The first number
+        b: The second number
+
+    Returns:
+        The addition of  a  and b number
+    """
+    if (type(a) not in (int, float)):
+        raise TypeError("a must be an integer")
+    if (type(b) not in (int, float)):
+        raise TypeError("b must be an integer")
+    if (type(a) is float):
+        a = int(a)
+    if (type(b) is float):
+        b = int(b)
+    return (a + b)
+
+
+if __name__ == '__main__':
+    doctest.testfile('tests/0-add_integer.txt')
