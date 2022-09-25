4d120f2e28f7d6b72515a371d6ef397cb4fdcfd2
UNICA-FIME <juancarlosfime@gmail.com>
2022-09-25 15:14:36 +0000

    function that prints string

---
+++ python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""This module implements a function that
+allows to print an string
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """This a function that print two string
+
+    Arg:
+        first_name: This first date string
+        last_name: This second date string
+    """
+    if (type(first_name) is not str):
+        raise TypeError("first_name must be a string")
+    if (type(last_name) is not str):
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))
