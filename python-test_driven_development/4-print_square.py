b8eafa979ca2510b9d81537d8e8b11a72d6adc29
UNICA-FIME <juancarlosfime@gmail.com>
2022-09-27 17:07:06 +0000

    correction error

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -6,9 +6,12 @@ def print_square(size):
     """This function print area square with #"""
     if (type(size) is not int):
         raise TypeError("size must be an integer")
-    elif (size < 0):
+    if (size < 0):
         raise ValueError("size must be >= 0")
-    elif (type(size) is float and size < 0):
+    if (type(size) is float and size < 0):
         raise TypeError("size must be an integer")
-    new_list = ["#" * size for i in range(0, size)]
-    print("\n".join(new_list))
+    if(size == 0):
+        return (None)
+    else:
+        new_list = ["#" * size for i in range(0, size)]
+        print("\n".join(new_list))
