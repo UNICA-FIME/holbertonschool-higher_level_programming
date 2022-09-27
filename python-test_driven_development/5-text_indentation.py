4105377f84426b51b0541ee15351257dcb8151db
UNICA-FIME <juancarlosfime@gmail.com>
2022-09-27 19:10:36 +0000

    Text indentation

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""This is a module for print a text"""
+
+
+def text_indentation(text):
+    """ This is a function that print a string
+
+    Arg : text is a string
+
+    """
+    if (type(text) is not str):
+        raise TypeError("text must be a string")
+    i = 0
+    while(i < len(text)):
+        if text[i] in [".", "?", ":"]:
+            print(text[i], end="")
+            print()
+            print()
+            i += 1
+        else:
+            print(text[i], end="")
+        i += 1
