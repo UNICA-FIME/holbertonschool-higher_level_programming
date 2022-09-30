d8347d2fac40f0bd2f6baa7da3f85c0c73b1807a
UNICA-FIME <juancarlosfime@gmail.com>
2022-09-30 05:43:24 +0000

    same object

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""This is a module create un type class"""
+
+
+def is_same_class(obj, a_class):
+    """Thi is a function object argument is an
+    instance of the classinfo argument
+
+    """
+    if (type(obj) is a_class):
+        return True
+    else:
+        return False
