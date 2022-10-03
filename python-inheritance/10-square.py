02fb6ea00cbc9987ddceae64d969bc3d0991833e
UNICA-FIME <juancarlosfime@gmail.com>
2022-10-03 16:54:12 +0000

    square

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""This a module create that type class Square"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """This is a class type square"""
+    def __init__(self, size):
+        """This is a function what calculate that area"""
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        return (self.__size**2)
