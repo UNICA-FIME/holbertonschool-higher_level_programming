a0ffeb0babae38972a2f2eead13e7019fd6947d2
UNICA-FIME <juancarlosfime@gmail.com>
2022-10-04 03:36:27 +0000

    JSON string to Object

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This is a module create method"""
+import json
+
+
+def from_json_string(my_str):
+    """This is a method create JSON string to Object"""
+    return (json.loads(my_str))
