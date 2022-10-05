a1b3f055ab85932ac76dd94ab85e2621690ddb49
UNICA-FIME <juancarlosfime@gmail.com>
2022-10-05 04:20:36 +0000

    Load, add, save

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""This is a  module create script"""
+
+import json
+from sys import argv
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+filename = "add_item.json"
+if len(argv) == 1:
+    list_new = []
+    save_to_json_file(list_new, filename)
+else:
+    obj_json = load_from_json_file(filename)
+    for i in argv[1:]:
+        obj_json.append(i)
+    save_to_json_file(obj_json, filename)
