c02395be93e802f355c7caf5d2d8b45dd8875bab
UNICA-FIME <juancarlosfime@gmail.com>
2022-10-05 05:06:37 +0000

    Class to JSON

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This is a module create a method"""
+import json
+
+
+def class_to_json(obj):
+    """This is a function create base date JSON"""
+    data_json = obj.__dict__
+    return data_json
