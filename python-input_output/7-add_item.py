8a844fee18f0f361592d6360d3515b28ddae511a
UNICA-FIME <juancarlosfime@gmail.com>
2022-10-05 04:50:33 +0000

    correction error

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -10,8 +10,11 @@ filename = "add_item.json"
 if len(argv) == 1:
     list_new = []
     save_to_json_file(list_new, filename)
-else:
-    obj_json = load_from_json_file(filename)
+if len(argv) > 1:
+    try:
+        obj_json = load_from_json_file(filename)
+    except Exception:
+        obj_json = []
     for i in argv[1:]:
         obj_json.append(i)
     save_to_json_file(obj_json, filename)
