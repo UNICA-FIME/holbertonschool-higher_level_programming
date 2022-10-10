7a237ff95d27122002a31fdba7a74d00361bc3fc
UNICA-FIME <juancarlosfime@gmail.com>
2022-10-10 17:26:54 +0000

    change

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -9,10 +9,10 @@ class Rectangle(Base):
     def __init__(self, width, height, x=0, y=0, id=None):
         """constructor"""
         super().__init__(id)
-        self.__width = width
-        self.__height = height
-        self. __x = x
-        self.__y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
 
     @property
     def width(self):
@@ -37,19 +37,39 @@ class Rectangle(Base):
     @width.setter
     def width(self, value):
         """Change value pravite"""
-        self.__width = value
+        if (type(value) is not int):
+            raise TypeError("width must be an integer")
+        elif (value <= 0):
+            raise ValueError("width must be > 0")
+        else:
+            self.__width = value
 
     @height.setter
     def height(self, value):
         """Change value pravite"""
-        self.__height = value
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        elif (value <= 0):
+            raise ValueError("height must be > 0")
+        else:
+            self.__height = value
 
     @x.setter
     def x(self, value):
         """Change value pravite"""
-        self.__x = value
+        if type(value) is not int:
+            raise TypeError("x must be an integer")
+        elif (value < 0):
+            raise ValueError("x must be >= 0")
+        else:
+            self.__x = value
 
     @y.setter
     def y(self, value):
         """Change value pravite"""
-        self.__y = value
+        if (type(value) is not int):
+            raise TypeError("y must be an integer")
+        elif (value < 0):
+            raise ValueError("y must be >= 0")
+        else:
+            self.__y = value
