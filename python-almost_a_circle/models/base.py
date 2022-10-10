fac9e5eb544a78b90e83053891d92f33e2a8335b
UNICA-FIME <juancarlosfime@gmail.com>
2022-10-10 20:38:20 +0000

    json string

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -14,6 +14,7 @@ class Base(object):
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         """Json representation string"""
         if list_dictionaries is None or len(list_dictionaries) == 0:
