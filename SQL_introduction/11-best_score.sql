a94712aa2dd154b8a5de0724373a97478a52bf86
UNICA-FIME <juancarlosfime@gmail.com>
2022-11-03 23:14:43 +0000

    Select the best

---
+++ SQL_introduction/11-best_score.sql
@@ -0,0 +1,4 @@
+sts all records with score >= 10
+--  display both the score and the name (in this order)
+-- Records should be ordered by score (top first)
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
