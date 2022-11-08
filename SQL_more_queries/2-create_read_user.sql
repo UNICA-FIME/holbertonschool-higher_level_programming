3b4f380e65539a3b2adb5b83ad6501b6085798d9
UNICA-FIME <juancarlosfime@gmail.com>
2022-11-08 23:02:51 +0000

    change privilege

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -3,4 +3,4 @@
 --  asigned only SELECT privilege in the database 
 CREATE SCHEMA IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON *.* TO user_0d_2@localhost;
+GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
