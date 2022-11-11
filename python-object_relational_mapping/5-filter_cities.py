c57e87fb89943f261efe2d85bf5557deb5b82823
UNICA-FIME <juancarlosfime@gmail.com>
2022-11-11 21:22:01 +0000

    All cities by state

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -6,14 +6,15 @@
 import MySQLdb
 import sys
 
-conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+if __name__ == "__main__":
+    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                        passwd=sys.argv[2], db=sys.argv[3])
-cur = conn.cursor()
-cur.execute("SELECT cities.name FROM states \
+    cur = conn.cursor()
+    cur.execute("SELECT cities.name FROM states \
              INNER JOIN cities ON states.id=cities.state_id \
              WHERE states.name=%s", (sys.argv[4],))
-query_row = cur.fetchall()
-for item in range(0, len(query_row)):
-    if item < len(query_row)-1:
-        print("{}".format(query_row[item][0]), end=", ")
-print("{}".format(query_row[item][0]))
+    query_row = cur.fetchall()
+    for item in range(0, len(query_row)):
+        if item < len(query_row)-1:
+            print("{}".format(query_row[item][0]), end=", ")
+    print("{}".format(query_row[item][0]))
