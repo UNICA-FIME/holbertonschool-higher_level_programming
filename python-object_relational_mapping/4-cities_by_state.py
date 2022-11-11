72b092864282559855c455ecd1d467d87cfb64f2
UNICA-FIME <juancarlosfime@gmail.com>
2022-11-11 18:49:17 +0000

    Cities by states

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""script that lists all cities from the database hbtn_0e_4_usa
+"""
+script that lists all cities from the database hbtn_0e_4_usa
 Lists all cities of the database hbtn_0e_4_usa, ordered by city id.
 Usage: ./4-cities_by_state.py <mysql username> \
                               <mysql password> \
