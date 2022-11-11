e0d1efffce4db7bd4d9a8d699858cbc3ef141514
UNICA-FIME <juancarlosfime@gmail.com>
2022-11-11 14:58:26 +0000

    1. Filter states

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -8,13 +8,13 @@ list_1 convierte el objeto en un lista iterable
 """
 import sys
 import MySQLdb
-
-db_connection = MySQLdb.connect(host="localhost", port=3306,
+if __name__ == "__main__":
+    db_connection = MySQLdb.connect(host="localhost", port=3306,
                                 user=sys.argv[1], passwd=sys.argv[2],
                                 db=sys.argv[3])
-cursor = db_connection.cursor()
-cursor_leng = cursor.execute("SELECT * FROM states ORDER BY states.id")
-list_1 = list(cursor)
-for item in range(0, cursor_leng):
-    if list_1[item][1][0] == 'N':
-        print(list_1[item])
+    cursor = db_connection.cursor()
+    cursor_leng = cursor.execute("SELECT * FROM states ORDER BY states.id")
+    list_1 = list(cursor)
+    for item in range(0, cursor_leng):
+        if list_1[item][1][0] == 'N':
+            print(list_1[item])
