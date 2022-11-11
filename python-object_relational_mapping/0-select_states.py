3abe7c609c9a6e0c412339c6c53300a39041a9d2
UNICA-FIME <juancarlosfime@gmail.com>
2022-11-11 02:02:12 +0000

    Get all states

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -9,7 +9,9 @@ cur.execute rum query
 import sys
 import MySQLdb
 if __name__ == '__main__':
-    db_connection = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    db_connection = MySQLdb.connect(host='localhost', port=3306,
+                                    user=sys.argv[1], passwd=sys.argv[2],
+                                    db=sys.argv[3])
     cur = db_connection.cursor()
     cur.execute("SELECT * FROM states ORDER BY states.id ASC")
     for item in cur:
