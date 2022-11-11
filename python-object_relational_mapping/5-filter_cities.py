c854654d5b983d55c2a0eb005b7ff4aef289a476
UNICA-FIME <juancarlosfime@gmail.com>
2022-11-11 21:20:08 +0000

    All cities by state

---
+++ python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+ script that takes in the name of a state as an argument and lists
+ all cities of that state, using the database hbtn_0e_4_usa
+"""
+import MySQLdb
+import sys
+
+conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                       passwd=sys.argv[2], db=sys.argv[3])
+cur = conn.cursor()
+cur.execute("SELECT cities.name FROM states \
+             INNER JOIN cities ON states.id=cities.state_id \
+             WHERE states.name=%s", (sys.argv[4],))
+query_row = cur.fetchall()
+for item in range(0, len(query_row)):
+    if item < len(query_row)-1:
+        print("{}".format(query_row[item][0]), end=", ")
+print("{}".format(query_row[item][0]))
