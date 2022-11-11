9a18c24313e182605b144881cf1483a197bc209d
UNICA-FIME <juancarlosfime@gmail.com>
2022-11-11 18:53:38 +0000

    correction error

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -6,16 +6,16 @@ Usage: ./4-cities_by_state.py <mysql username> \
                               <mysql password> \
                               <database name>
 """
-import MySQLdb
 import sys
+import MySQLdb
 
 if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost", port=3306,
                            user=sys.argv[1], passwd=sys.argv[2],
                            db=sys.argv[3])
     cur = conn.cursor()
-    cur.execute("SELECT cities.id, cities.name, states.name
-                FROM cities INNER JOIN states ON states.id=cities.state_id
+    cur.execute("SELECT cities.id, cities.name, states.name \
+                FROM cities INNER JOIN states ON states.id=cities.state_id \
                 ORDER BY cities.id")
     query_rows = cur.fetchall()
     for row in query_rows:
