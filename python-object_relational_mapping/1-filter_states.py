f10af3643065e7a371d959369aeff7720fc62ac1
UNICA-FIME <juancarlosfime@gmail.com>
2022-11-11 15:00:29 +0000

    correction error

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -10,8 +10,8 @@ import sys
 import MySQLdb
 if __name__ == "__main__":
     db_connection = MySQLdb.connect(host="localhost", port=3306,
-                                user=sys.argv[1], passwd=sys.argv[2],
-                                db=sys.argv[3])
+                                    user=sys.argv[1], passwd=sys.argv[2],
+                                    db=sys.argv[3])
     cursor = db_connection.cursor()
     cursor_leng = cursor.execute("SELECT * FROM states ORDER BY states.id")
     list_1 = list(cursor)
