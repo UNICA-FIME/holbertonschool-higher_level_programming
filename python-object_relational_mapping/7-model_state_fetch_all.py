6adc363cb206e3e2d38b16fb1dfc35232568b6bd
UNICA-FIME <juancarlosfime@gmail.com>
2022-11-13 22:54:31 +0000

    All states via SQLAlchemy

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -8,17 +8,17 @@ session.query(State).order_by(State.id).all()
  session.query(State).order_by(State.id): ordena por id
  session.query(State).order_by(State.id).all(): selecciona todo la columna y ordena.
 """
-import sys
-from model_state import State
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 
-engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                       .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                       pool_pre_ping=True)
-
-Session = sessionmaker(bind=engine)
-session = Session()
-q = session.query(State).order_by(State.id).all()
-for state in q:
-    print("{}: {}".format(state.id, state.name))
+if __name__ == '__main__':
+    import sys
+    from model_state import State
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                            pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    q = session.query(State).order_by(State.id).all()
+    for state in q:
+        print("{}: {}".format(state.id, state.name))
