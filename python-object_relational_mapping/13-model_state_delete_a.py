4b905176c27325bebcc32a2a050ff47e95e07dad
UNICA-FIME <juancarlosfime@gmail.com>
2022-11-14 03:12:16 +0000

    Delete states

---
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+firts(): returns the first result of the query or None,
+if there are no rows in the result.
+"""
+
+if __name__ == '__main__':
+    import sys
+    from model_state import Base, State
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker, Session
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Session = sessionmaker(engine)
+    session = Session()
+    q = session.query(State.id, State.name).order_by(State.id).all()
+    for item in range(0, len(q)):
+        j = 0
+        for i in range(0, len(q[item][1])):
+            if q[item][1][i] == 'a' and j == 0:
+                d = session.query(State).filter(State.id == q[item][0]).one()
+                j = j + 1
+                session.delete(d)
+    session.commit()
