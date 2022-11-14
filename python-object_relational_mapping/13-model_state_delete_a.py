5182df35b73485a64f9b0964c60b88bf01cc7bef
UNICA-FIME <juancarlosfime@gmail.com>
2022-11-14 03:33:30 +0000

    correction error

--- python-object_relational_mapping/13-model_state_delete_a.py
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,26 +1,21 @@
 #!/usr/bin/python3
-"""
-firts(): returns the first result of the query or None,
-if there are no rows in the result.
-"""
-
+""" This script lists all State objects with the name passed as
+argument from database hbtn_0e_6_usa """
 if __name__ == '__main__':
-    import sys
-    from model_state import Base, State
     from sqlalchemy import create_engine
-    from sqlalchemy.orm import sessionmaker, Session
+    from sys import argv
+    from model_state import Base, State
+    from sqlalchemy.orm import sessionmaker
 
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
-    Session = sessionmaker(engine)
+    engine = create_engine("""mysql+mysqldb://{}:{}@localhost/{}"""
+                           .format(argv[1], argv[2], argv[3]))
+
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
     session = Session()
-    q = session.query(State.id, State.name).order_by(State.id).all()
-    for item in range(0, len(q)):
-        j = 0
-        for i in range(0, len(q[item][1])):
-            if q[item][1][i] == 'a' and j == 0:
-                d = session.query(State).filter(State.id == q[item][0]).one()
-                j = j + 1
-                session.delete(d)
+    states = session.query(State)
+    states = states.filter(State.name.like('%a%')).order_by(State.id)
+    for state in states:
+        session.delete(state)
     session.commit()
+    session.close()
