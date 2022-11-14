8f54dc55be559ff55fe137a18dc8d93e2a71126b
UNICA-FIME <juancarlosfime@gmail.com>
2022-11-14 02:55:16 +0000

    Update a state

---
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+firts(): returns the first result of the query or None,
+if there are no rows in the result.
+"""
+
+if __name__ == '__main__':
+    import sys
+    from model_state import Base, State
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker, Session
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Session = sessionmaker(engine)
+    session = Session()
+    q = session.query(State).filter(State.id == 2).first()
+    q.name = "New Mexico"
+    session.commit()
