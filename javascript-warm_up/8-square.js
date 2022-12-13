d718f5ad38e0029665b3830966f321d6ba417b01
user name <user email>
2022-12-13 04:34:05 +0000

    fix

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
-const s = parseInt(process.argv[2]);
-if (isNaN(s) || process.argv.length <= 2) {
+if (isNaN(parseInt(process.argv[2]))) {
   console.log('Missing size');
-}
-if (s > 0) {
-  const arr = 'X,'.repeat(s).split(',').splice(0, s);
-  console.log(arr.map((x) => x.repeat(s)).join('\n'));
+} else {
+  for (let i = 0; i < parseInt(process.argv[2]); i++) {
+    console.log('X'.repeat(parseInt(process.argv[2])));
+  }
 }
