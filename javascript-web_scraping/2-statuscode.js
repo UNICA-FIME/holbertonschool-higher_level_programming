a132f70ec992da4675ac7ffce7f988b05d568ded
user name <user email>
2022-12-16 22:24:20 +0000

    status code

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const request = require('request');
 const url = process.argv[2];
-request((url), function (response) {
+request((url), function ( error, response) {
   console.log('code: ', response && response.statusCode);
 });
