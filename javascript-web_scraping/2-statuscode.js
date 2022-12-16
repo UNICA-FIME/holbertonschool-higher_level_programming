a4334926cdf3b1f1172696d41f2b9c31d23fde7d
user name <user email>
2022-12-16 22:52:45 +0000

    change error

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -1,6 +1,9 @@
 #!/usr/bin/node
 const request = require('request');
 const url = process.argv[2];
-request(url, function (error, response, body) {
+request((url), function (error, response) {
+  if (error) {
+    console.log(error);
+  }
   console.log('code:', response && response.statusCode);
 });
