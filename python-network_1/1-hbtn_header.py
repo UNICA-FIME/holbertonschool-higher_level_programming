65a3d37dd9d1f51efe3dd6c7d5106a0fc77dd316
user name <user email>
2023-01-29 22:40:40 +0000

    Response header value

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -3,9 +3,9 @@
  script that takes in a URL, sends a request to the URL and displays the
  value of the X-Request-Id variable found in the header of the response.
 """
-import sys
-import urllib.request
-with urllib.request.urlopen(sys.argv[1]) as response:
-    html = dict(response.headers)
-
-print(html['X-Request-Id'])
+if __name__ == "__main__":
+    import sys
+    import urllib.request
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        html = dict(response.headers)
+    print(html['X-Request-Id'])
